8b435ee8bf4fdba1471a211831d600b5fbf5752d
hafizova04 <hafizovaaf@mer.ci.nsu.ru>
2022-10-14 22:44:10 +0700

    попадание в область

---
+++ 3.cs
@@ -0,0 +1,17 @@
+using System;
+while (true)
+{
+    Console.WriteLine("Введите значение по x");
+    double = = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите значение по y");
+    double  = = double.Parse(Console.ReadLine());
+    1 = radius = 1;
+
+    if ((x <= 0) && (y <= 0) && (x * x + y * y <= radius  * radius))
+    {
+        Console.WriteLine("Точка принадлежит III четверти");
+    }
+    else
+        Console.WriteLine("Точка не принадлежит III четверти");
+}
+
